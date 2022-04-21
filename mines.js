66af5f75e2c97a41a6c98f77cfffbc557609bf18
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-04-21 18:10:32 -0300

    placed mines on board, clicked tile will show surrounding mines

--- mines.js
+++ mines.js
@@ -1,6 +1,5 @@
 let board = {
   domBoard: document.querySelector(".board"),
-  domTiles: undefined,
   width: 20,
   height: 10,
   
@@ -13,16 +12,15 @@ let board = {
         tile.classList = "tile";
         tile.setAttribute("x",`${j}`);
         tile.setAttribute("y",`${i}`);
+        tile.style.aspectRatio = "1/1";
 
         tile.addEventListener("click", () => {
-          // tile.classList.add("selected");
-          checkSurroundings(j,i);
+          checkForMines(j,i);
         });
         row.appendChild(tile);
       };
     };
     this.domBoard.style.aspectRatio = `${board.width}/${board.height}`;
-    this.domTiles = document.querySelectorAll(".tile");
   },
 }
 
@@ -34,8 +32,14 @@ let mines = {
     for (let i = 0; i < this.quantity; i++) {
       let x = rand(board.width);
       let y = rand(board.height);
-      this.positions.push(`[${x},${y}]`);
+      //note: check if there's a mine first before placing another
+      this.positions.push([x,y]);
+      //test, show mines on board
+      const test = document.querySelector(`[x="${x}"][y="${y}"]`)
+      test.classList.add("testing")
+      ///////////////////////////
     };
+    
   },
 };
 
@@ -46,11 +50,77 @@ function rand(n){
   return Math.floor(Math.random()*n)
 };
 
+function checkForMines(x,y){
+  let surrounding = [];
+  let foundMines = 0;
+  surrounding.push([x,y-1]); //N
+  surrounding.push([x+1,y-1]); //NE
+  surrounding.push([x+1,y]); //E
+  surrounding.push([x+1,y+1]); //SE
+  surrounding.push([x,y+1]); //S
+  surrounding.push([x-1,y+1]); //SW
+  surrounding.push([x-1,y]); //W
+  surrounding.push([x-1,y-1]); //NW
+
+  surrounding.forEach(t => {
+    mines.positions.forEach(m =>{
+      if(t.toString() == m.toString()){
+        foundMines++;
+      };
+    });
+  });
+  const clickedTile = document.querySelector(`[x="${x}"][y="${y}"]`);
+  clickedTile.classList.add("selected");
+  clickedTile.innerText = foundMines;
+
+  let color = "gray";
+  switch(foundMines){
+    case 0:
+      clickedTile.innerText = "";
+      break;
+    case 1:
+      color = "blue";
+      break;
+    case 2:
+      color = "green";
+      break;
+    case 3:
+      color = "red";
+      break;
+    case 4:
+      color = "darkblue";
+      break;
+      case 5:
+      color = "darkred";
+      break;
+    case 6:
+      break;
+    case 7:
+      break;
+    case 8:
+      break;
+    default:
+      alert("an error ocurred");
+      break;
+    }
+    clickedTile.style.color = color;
+
+};
+
+
+//unused
 function checkSurroundings(x,y){
   let tileC = document.querySelector(`[x="${x}"][y="${y}"]`);
   let tileN = document.querySelector(`[x="${x}"][y="${y-1}"]`);
+  let tileNE = document.querySelector(`[x="${x+1}"][y="${y-1}"]`);
+  let tileE = document.querySelector(`[x="${x+1}"][y="${y}"]`);
+  let tileSE = document.querySelector(`[x="${x+1}"][y="${y+1}"]`);
+  let tileS = document.querySelector(`[x="${x}"][y="${y+1}"]`);
+  let tileSW = document.querySelector(`[x="${x-1}"][y="${y+1}"]`);
+  let tileW = document.querySelector(`[x="${x-1}"][y="${y}"]`);
+  let tileNW = document.querySelector(`[x="${x-1}"][y="${y-1}"]`);
   
-  [tileC, tileN].forEach(t => {
+  [tileC,tileN,tileNE,tileE,tileSE,tileS,tileSW,tileW,tileNW].forEach(t => {
     t.classList.add("selected");
   });
 };
